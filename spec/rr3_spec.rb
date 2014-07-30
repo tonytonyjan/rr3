435656b44b61475c0edc7fded46f6ee129c38e71
Tony Jian <tonytonyjan@gmail.com>
2014-07-30 14:52:07 +0800

    update spec

--- spec/rr3_spec.rb
+++ spec/rr3_spec.rb
@@ -6,17 +6,25 @@ describe Rr3 do
     @tree = Rr3::Tree.new(10)
   end
 
-  it 'works' do
+  it 'matches' do
     @tree.insert_path "/foo/bar"
     @tree.compile!
-    expect(@tree.match("/foo/bar")).to be nil
-    expect(@tree.match("/foo/barr")).to be nil
+    expect(@tree.match("/foo/bar")).to be_nil
+    expect(@tree.match("/foo/barr")).to be_nil
+  end
+
+  it 'dumps' do
+    @tree.insert_path "/foo/bar"
+    @tree.compile!
     @tree.dump(0)
   end
 
   it 'should pass any data' do
     @tree.insert_path "/foo/bar", 9527
-    expect(@tree.match("/foo/bar")).to be 9527
+    @tree.insert_path "/hello", "Hello"
+    @tree.compile!
+    expect(@tree.match("/foo/bar")).to eql 9527
+    expect(@tree.match("/hello")).to eql "Hello"
   end
 
   it 'should returns root node' do
