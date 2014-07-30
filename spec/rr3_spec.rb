7e5c82a897aa1553243860314d7091229a407c20
Tony Jian <tonytonyjan@gmail.com>
2014-07-30 16:47:05 +0800

    update spec

--- spec/rr3_spec.rb
+++ spec/rr3_spec.rb
@@ -5,21 +5,22 @@ describe Rr3 do
   before(:all)do
     @tree = Rr3::Tree.new(10)
     @tree.insert_path "/bar"
-    @tree.insert_path "/zoo", 9527
-    @tree.insert_path "/foo/bar", 9527
-    @tree.insert_path "/post/{id}", 9527
-    @tree.insert_path "/user/{id:\\d+}", 9527
+    @tree.insert_path "/zoo", "test"
+    @tree.insert_path "/foo/bar", a: 1
+    @tree.insert_path "/post/{id}", [1]
+    class A; attr_accessor :bar; end;
+    @tree.insert_path "/user/{id:\\d+}", A.new.tap{|a| a.bar = 1}
     @tree.compile!
   end
 
   it 'matches' do
-    expect(@tree.match("/bar")).to be nil
-    expect(@tree.match("/zoo")).to be 9527
-    expect(@tree.match("/foo/bar")).to be 9527
-    expect(@tree.match("/post/1234")).to be 9527
-    expect(@tree.match("/user/1234")).to be 9527
-    expect(@tree.match("/user/xxx")).to be false
-    expect(@tree.match("/asd")).to be false
+    expect(@tree.match("/bar")).to be_nil
+    expect(@tree.match("/zoo")).to eql "test"
+    expect(@tree.match("/foo/bar")).to eql a: 1
+    expect(@tree.match("/post/1234")).to eql [1]
+    expect(@tree.match("/user/1234").bar).to eql 1
+    expect(@tree.match("/user/xxx")).to eql false
+    expect(@tree.match("/asd")).to eql false
   end
 
   it 'dumps' do
