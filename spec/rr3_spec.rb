33bf2d4a9c6d0b37c322899b57ec7bbdd543232a
Tony Jian <tonytonyjan@gmail.com>
2014-07-30 15:11:23 +0800

    update spec

--- spec/rr3_spec.rb
+++ spec/rr3_spec.rb
@@ -2,29 +2,28 @@ $: << File.expand_path('../../ext', __FILE__)
 require 'spec_helper'
 
 describe Rr3 do
-  before(:each)do
+  before(:all)do
     @tree = Rr3::Tree.new(10)
+    @tree.insert_path "/bar"
+    @tree.insert_path "/zoo", 9527
+    @tree.insert_path "/foo/bar", 9527
+    @tree.insert_path "/post/{id}", 9527
+    @tree.insert_path "/user/{id:\\d+}", 9527
+    @tree.compile!
   end
 
   it 'matches' do
-    @tree.insert_path "/foo/bar"
-    @tree.compile!
-    expect(@tree.match("/foo/bar")).to be_nil
-    expect(@tree.match("/foo/barr")).to be_nil
+    expect(@tree.match("/bar")).to be_nil
+    expect(@tree.match("/asdf")).to be_nil
   end
 
   it 'dumps' do
-    @tree.insert_path "/foo/bar"
-    @tree.compile!
     @tree.dump(0)
   end
 
   it 'should pass any data' do
-    @tree.insert_path "/foo/bar", 9527
-    @tree.insert_path "/hello", "Hello"
-    @tree.compile!
     expect(@tree.match("/foo/bar")).to eql 9527
-    expect(@tree.match("/hello")).to eql "Hello"
+    expect(@tree.match("/post/123")).to eql 9527
   end
 
   it 'should returns root node' do
